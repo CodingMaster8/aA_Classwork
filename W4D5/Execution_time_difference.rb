f79ded41990b5e56db80095d1748ca4b5a62f141
jason <jmasters1000@yahoo.com>
2021-02-12 14:35:54 -0800

    Complete largest_contiguous_subsum problem, phase 1

--- W4D5/Execution_time_difference.rb
+++ W4D5/Execution_time_difference.rb
@@ -31,40 +31,53 @@ end
 #Largest Contiguous Sub-sum
 #You have an array of integers and you want to find the largest contiguous (together in sequence) sub-sum. Find the sums of all contiguous sub-arrays and return the max.
 
-def largest_contiguous_sub_sum(list)
-  sum = 0
-  subs2 = sub_sets(list)
-  subs2
-  subs2.each do |sub_arr|
-    if sub_arr.sum > sum 
-      sum = sub_arr.sum 
+# def largest_contiguous_sub_sum(list)
+#   sum = 0
+#   subs2 = sub_sets(list)
+#   subs2
+#   subs2.each do |sub_arr|
+#     if sub_arr.sum > sum 
+#       sum = sub_arr.sum 
+#     end
+#   end
+
+#   sum
+# end
+
+# def sub_sets(array)
+#   return [[]] if array.empty?
+#   subs = largest_contiguous_sub_sum(array[0...-1])
+  
+#   subs.concat(subs.map { |sub| sub + [array.last] })
+
+# end
+
+
+def largest_contiguous_subsum(arr)
+  new_arr = []
+
+  arr.each_with_index do |num1, idx1|
+    new_arr << [num1]
+    arr.each_with_index do |num2, idx2|
+      if idx2 > idx1 
+        new_arr << arr[idx1..idx2]
+      end
     end
   end
+  sub_arr = new_arr
 
-  sum
-end
-
-def sub_sets(array)
-  return [[]] if array.empty?
-  subs = largest_contiguous_sub_sum(array[0...-1])
-  
-  subs.concat(subs.map { |sub| sub + [array.last] })
-
+  sub_sum = sub_arr.map { |sub| sub.sum }
+  sub_sum.max
 end
 
 
 
-list = [5, 3, -7]
-p largest_contiguous_sub_sum(list) # => 8
-
-  # possible sub-sums
-    # [5]           # => 5
-    # [5, 3]        # => 8 --> we want this one
-    # [5, 3, -7]    # => 1
-    # [3]           # => 3
-    # [3, -7]       # => -4
-    # [-7]          # => -7
+# list = [5, 3, -7]     #=> [[5], [5, 3], [5, 3, -7], [3], [3, -7], [-7]]
+# p largest_contiguous_subsum(list) # => 8
 
 
 # list = [2, 3, -6, 7, -6, 7]
-# largest_contiguous_sub_sum(list) # => 8 (from [7, -6, 7])
+# p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list) # => -1 (from [-1])
