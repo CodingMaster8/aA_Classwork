cf0b9b55aff9240b3b11c99a3fb504b28cc2c586
jason <jmasters1000@yahoo.com>
2021-02-24 16:14:28 -0800

    Create migrations and seeds for users in First Routes project

--- W6D3/First_Routes/db/seeds.rb
+++ W6D3/First_Routes/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  User.create([{name: 'Martin', email: 'martin28@dog.com'}, {name: 'Petunia', email: 'petuniaisthebest@gmail.com'}])
