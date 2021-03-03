8e6ecaadb56895035b732738a119d5e186569c43
jason <jmasters1000@yahoo.com>
2021-03-02 20:34:16 -0800

    Work on Music App project

---
+++ W7D2/music_app/app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ApplicationRecord
+
+  validates :email, :session_token, uniqueness: true
+  validates :email, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+
+  def generate_session_token
+    token = SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64(16)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+end
