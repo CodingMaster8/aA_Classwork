0f3dce3bb96547eb2fa753d9e3bbcee6b7fe594e
jason <jmasters1000@yahoo.com>
2021-03-08 10:22:34 -0800

    Finish user auth

--- W8D1/Reddit_Clone/app/controllers/sessions_controller.rb
+++ W8D1/Reddit_Clone/app/controllers/sessions_controller.rb
@@ -1,16 +1,31 @@
 class SessionsController < ApplicationController
 
+  before_action :require_logged_in, only: [:destroy]
+
   def create
     @user = User.find_by_credentials(
       params[:user][:username],
       params[:user][:password]
     )
+
     if @user
       login!(@user)
       redirect_to users_url
-      
+    else
+      flash.now[:errors] = ['Username and/or Password is incorrect']
+      render :new
+    end
+
   end
 
+  def new
+    render :new
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
 
 
 end
